df06cc7f5d53f3474af49bab0d7dc497b38a5f9f
Jean Jordaan <jean.jordaan@gmail.com>
2012-05-02 20:49:52 +0700

    Docstring edits.

--- youraddon/interfaces.py
+++ youraddon/interfaces.py
@@ -1,6 +1,4 @@
-"""
-
-	For more information see
+"""For more information see
 
 	* http://collective-docs.readthedocs.org/en/latest/components/interfaces.html
 
@@ -12,18 +10,16 @@ from zope.interface import Interface
 from plone.theme.interfaces import IDefaultPloneLayer
 
 class IThemeSpecific(IDefaultPloneLayer):
-    """
-    Marker interface that defines a browser layer against which you can register views and viewlets.
+    """Marker interface: defines a browser layer against which you can register views and viewlets.
 
-    When your theme is selected this layer becomes active and all overrides associated with
-    override default Plone render actions.
+    When your theme is selected, this layer becomes active and all overrides
+    associated with it will override default Plone render actions.
     """
 
 class IAddonSpecific(IDefaultPloneLayer):    
-    """
-    Marker interface that defines a browser layer against which you can register views and viewlets.
+    """Marker interface: defines a browser layer against which you can register views and viewlets.
 
-    When your add-on is installed this layer becomes active and all overrides associated with
-    override default Plone render actions.
+    When your add-on is installed, this layer becomes active and all
+    overrides associated with it will override default Plone render actions.
     """
 
